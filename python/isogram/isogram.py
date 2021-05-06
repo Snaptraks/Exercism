fbbb7373034e07fb03cfe31811472c1b197b432b
Snaptraks <snaptraks@gmail.com>
2021-05-06 11:29:13 -0400

    Use sets in python/isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,11 +1,5 @@
-from collections import Counter
-
-
 def is_isogram(string):
-    if string == "":
-        return True
 
     stripped = string.replace(" ", "").replace("-", "").lower()
 
-    counter = Counter(stripped)
-    return counter.most_common(1)[0][1] == 1
+    return len(stripped) == len(set(stripped))
