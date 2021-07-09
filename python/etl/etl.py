2ae261ee3d1aa28c03f45e6ab41f049d32663677
Snaptraks <snaptraks@gmail.com>
2021-07-09 11:08:57 -0400

    Solve python/etl

---
+++ python/etl/etl.py
@@ -0,0 +1,7 @@
+def transform(legacy_data):
+    new_data = {}
+    for points, letters in legacy_data.items():
+        for letter in letters:
+            new_data[letter.lower()] = points
+
+    return new_data
