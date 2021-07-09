7173f85f699ea158755a203ff845117dd1c3af22
Snaptraks <snaptraks@gmail.com>
2021-07-09 10:42:33 -0400

    Improve python/space-age by removing repetitions

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,32 +1,20 @@
 class SpaceAge:
+    SECONDS_PER_YEAR = {planet: ratio * 31557600 for planet, ratio in {
+        "earth": 1.0,
+        "mercury": 0.2408467,
+        "venus": 0.61519726,
+        "mars": 1.8808158,
+        "jupiter": 11.862615,
+        "saturn": 29.447498,
+        "uranus": 84.016846,
+        "neptune": 164.79132,
+    }.items()}
+
     def __init__(self, seconds):
         self.seconds = seconds
-        self._earth_year = seconds / 31557600
 
-    def _round(self, value):
-        decimals = 2
-        return round(value, decimals)
+        for planet, seconds_per_year in self.SECONDS_PER_YEAR.items():
+            setattr(self, f"on_{planet}", self._on_planet(seconds_per_year))
 
-    def on_earth(self):
-        return self._round(self._earth_year)
-
-    def on_mercury(self):
-        return self._round(self._earth_year / 0.2408467)
-
-    def on_venus(self):
-        return self._round(self._earth_year / 0.61519726)
-
-    def on_mars(self):
-        return self._round(self._earth_year / 1.8808158)
-
-    def on_jupiter(self):
-        return self._round(self._earth_year / 11.862615)
-
-    def on_saturn(self):
-        return self._round(self._earth_year / 29.447498)
-
-    def on_uranus(self):
-        return self._round(self._earth_year / 84.016846)
-
-    def on_neptune(self):
-        return self._round(self._earth_year / 164.79132)
+    def _on_planet(self, seconds_per_year):
+        return lambda: round(self.seconds / seconds_per_year, 2)
