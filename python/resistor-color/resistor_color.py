468a7f1d0c6d1c382f046dd47849fbbe5003dd48
Snaptraks <snaptraks@gmail.com>
2021-09-16 11:42:36 -0400

    Solve python/resistor-color

---
+++ python/resistor-color/resistor_color.py
@@ -0,0 +1,20 @@
+COLOR_CODE = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+]
+
+
+def color_code(color):
+    return COLOR_CODE.index(color)
+
+
+def colors():
+    return COLOR_CODE
